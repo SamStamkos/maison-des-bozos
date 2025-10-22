8a261253e392763466a6662179d2e6136ef73664
Sam Stamkos <sleflaguais@gmail.com>
2025-10-22 13:27:11 -0400

    fix: resolve image display issues by replacing aspect-video with fixed height and adding debug logging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,7 @@ const Home: React.FC = () => {
         </div>
         <div className="grid grid-cols-6 gap-6 mt-24">
           <div className="relative col-span-3">
-            <div className="aspect-video rounded-xs overflow-hidden">
+            <div className="w-full h-64 rounded-xs overflow-hidden bg-gray-200">
               <img
                 src="/bozo.jpg"
                 alt="Maison des Bozos"
@@ -55,7 +55,7 @@ const Home: React.FC = () => {
             </div>
           </div>
           <div className="relative col-span-3">
-            <div className="aspect-video rounded-xs overflow-hidden">
+            <div className="w-full h-64 rounded-xs overflow-hidden bg-gray-200">
               <img
                 src="/museee.jpg"
                 alt="Maison des Bozos"
@@ -85,7 +85,11 @@ const Home: React.FC = () => {
             </p>
           </div>
           <div className="mt-12">
-            <img src="/musee.jpg" alt="Maison des Bozos" className="w-40" />
+            <img 
+              src="/musee.jpg" 
+              alt="Maison des Bozos" 
+              className="w-40" 
+            />
           </div>
         </section>
       </div>
